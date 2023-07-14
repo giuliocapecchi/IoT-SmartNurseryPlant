a991e0f5e29c1c85f7701b38ac5428c48c7b3a9a
fratifederico <121012872+fratifederico@users.noreply.github.com>
2023-07-14 19:05:43 +0200

    Update temperature_actuator.c
    Updatetd actuator, added button management

--- actuators/temperature_actuator.c
+++ actuators/temperature_actuator.c
@@ -1,10 +1,11 @@
+#include "contiki.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "contiki.h"
 #include "contiki-net.h"
 #include "coap-engine.h"
 #include "coap-blocking-api.h"
+#include "os/dev/button-hal.h"
 #include "etimer.h"
 #include "JSON_utility.h"
 #include "os/dev/leds.h"
@@ -29,12 +30,40 @@ static struct etimer et2;
 static int state = 2 ;
 static bool controlled = false;
 
+
 int get_state() {
     return state;
 }
 
 void set_state(int value){
     state = value;
+}
+
+void leds_management(){
+    if(state == 0){
+        leds_off(2);
+        leds_off(4);
+        leds_off(8);
+        leds_on(1);
+    }
+    else if(state == 1){
+        leds_off(1);
+        leds_off(2);
+        leds_off(4);
+        leds_on(8);
+    } 
+    else if(state == 2){
+        leds_off(1);
+        leds_off(2);
+        leds_off(8);
+        leds_on(4);
+    }
+    else if(state == 3){
+        leds_off(1);
+        leds_off(8);
+        leds_off(4);
+        leds_on(2);
+    }
 }
 
 void sleep(){ //used in the resource when forcing a state
@@ -57,7 +86,7 @@ void sleep(){ //used in the resource when forcing a state
 
 void client_response_handler(coap_message_t *response) {
     const uint8_t *chunk;
-    if (response == NULL) {
+    if (response==NULL) {
         puts("Request timed out");
         return;
     }
@@ -71,24 +100,15 @@ void client_response_handler(coap_message_t *response) {
 
     if (value > 28){
         // Actuator needs to be activated!
-        leds_off(4);
-        leds_off(8);
-        
-        leds_on(2); // rosso
         state = 3;
+        leds_management();
     }else if(value < 18 ){
         // Actuator needs to be activated!
-        leds_off(2);
-        leds_off(4);
-
-        leds_on(8); // blu
         state = 1;
+        leds_management();
     }else{
-        leds_off(8);
-        leds_off(2);
-
-        leds_on(4); // verde
         state = 2;
+        leds_management();
     }
 
 
@@ -103,7 +123,8 @@ AUTOSTART_PROCESSES(&temperature_actuator);
 // coap_endpoint_t -> represents an endpoint
 // coap_message_t -> represent the message
 PROCESS_THREAD(temperature_actuator, ev, data){
-
+    
+    button_hal_button_t *btn;
     static coap_endpoint_t server_ep;
     static coap_message_t request[1]; /* This way the packet can be treated as pointer as usual. */
    
@@ -112,12 +133,12 @@ PROCESS_THREAD(temperature_actuator, ev, data){
     coap_activate_resource(&res_actuator, "temperature_actuator");
     // Populate the coap_endpoint_t data structure
     coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);
-    
+   
     etimer_set(&et,10*CLOCK_SECOND);
     leds_on(1);
 
     while (1){
-
+        
         //leds_on(LEDS_RED); non funziona
         //leds_on(LEDS_GREEN); funziona
        
@@ -134,6 +155,22 @@ PROCESS_THREAD(temperature_actuator, ev, data){
        
        PROCESS_YIELD();
 
+        if(ev==button_hal_press_event){
+            state = (state+1)%4;
+            leds_management();
+            leds_on(1);                         //forced state, yellow led on
+            etimer_set(&et2,15*CLOCK_SECOND);
+            PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et2));
+        }
+
+        if(ev== button_hal_periodic_event){
+            btn = (button_hal_button_t *)data;
+            if(btn->press_duration_seconds > 5) {
+                state = 0;
+                leds_management();
+            } 
+        }
+
         if(state==0){ 
             etimer_reset(&et);
             continue;
@@ -146,14 +183,16 @@ PROCESS_THREAD(temperature_actuator, ev, data){
         }
         
         if(etimer_expired(&et)){
-         
+            
             // Prepare the message
             coap_init_message(request, COAP_TYPE_CON, COAP_GET, 0);
             coap_set_header_uri_path(request, service_url);
             // Issue the request in a blocking manner
             // The client will wait for the server to reply (or the transmission to timeout)
             // dopo sta richiesta si esegue l'handler poi si torna qui
+            
             COAP_BLOCKING_REQUEST(&server_ep, request, client_response_handler);
+            
             etimer_reset(&et);
         }
     }
