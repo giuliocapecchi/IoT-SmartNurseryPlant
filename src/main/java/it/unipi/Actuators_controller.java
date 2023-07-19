88fc742688e17d412aa3a34aea1e1ea5e88923a1
Giulio Capecchi <giuliocapecchi2000@gmail.com>
2023-07-19 21:17:10 +0200

    small correction

--- src/main/java/it/unipi/Actuators_controller.java
+++ src/main/java/it/unipi/Actuators_controller.java
@@ -63,7 +63,7 @@ public class Actuators_controller {
 
                     /*   IP     TOPIC    STATE   TIMESTAMP */
                     Connection connection = Database_manager.db_connection();
-                    String query = "INSERT INTO iotproject.actuators (ip, topic, state) VALUES ('"+ip_client+"','"+t+"',"+value+");";
+                    String query = "INSERT INTO iotproject.actuators (uri, topic, state) VALUES ('"+ip_client+"','"+t+"',"+value+");";
                     assert connection != null;
                     Database_manager.insert_executor(connection, query);
                     if(!Database_manager.close_connection(connection)) {
