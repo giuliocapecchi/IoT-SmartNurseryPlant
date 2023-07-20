36e909f58be7fdf772c7e6358531c3beaf5fe0ef
Giulio Capecchi <giuliocapecchi2000@gmail.com>
2023-07-20 16:22:15 +0200

    Small correction

--- Database_dump.sql
+++ Database_dump.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `actuators`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `actuators` (
-  `ip` varchar(255) DEFAULT NULL,
+  `uri` varchar(255) DEFAULT NULL,
   `topic` varchar(255) DEFAULT NULL,
   `state` varchar(50) DEFAULT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
